2fba14c84ce7dc5859c9ea2078cbf40bbd058678
Gloria Villa <gloriavilla@Glorias-MacBook-Pro.local>
2022-12-27 14:54:21 -0800

    reskill-wk-4-hw1

---
+++ script/index.js
@@ -0,0 +1,6 @@
+function player(name, tall, country) {
+    alert("Hello  " + name + "! You are " + tall + " inches tall and you are from " + country + "!");
+}
+
+
+// to do a quick test you can console log.  Then change to alert after.  player('gloria', 65, 'US')
